5a7fe05222bed6924a91e07c61f0eaaeb6c99b58
marouncz <marouncz@gmail.com>
2023-10-04 14:23:33 +0200

    added button functionality

---
+++ lab2/README.md
@@ -0,0 +1,7 @@
+1. Copy/paste [your solution](main.c) with two LEDs from Lab1 to `LAB2-GPIO_LIBRARY > src > main.c` source file. Compile (build) the project and note the first-version size in bytes.
+
+   | **Version** | **Size [B]** |
+   | :-- | :-: |
+   | Ver. 1: Arduino-style | 508 |
+   | Ver. 2: Registers | 182 |
+   | Ver. 3: Library functions | 182 |
--- lab2/lab2-gpio_library/src/main.c
+++ lab2/lab2-gpio_library/src/main.c
@@ -1,18 +1,99 @@
-#include <Arduino.h>
+/***********************************************************************
+ * 
+ * Blink two LEDs in Arduino-, register-, and library-style. Use 
+ * function from delay library to slow down blinking.
+ * 
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+ *
+ * Copyright (c) 2019 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ * 
+ **********************************************************************/
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+/* Defines -----------------------------------------------------------*/
+#define LED_GREEN 5   // PB5 is AVR pin where green on-board LED
+                        // is connected
+#define LED_RED 0     // Off-board LED
+#define BUTTON 1 //button connected to arduino pin 9
+#define SHORT_DELAY 50 // Delay in milliseconds
+#ifndef F_CPU
+# define F_CPU 16000000 // CPU frequency in Hz required for delay funcs
+#endif
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>     // AVR device-specific IO definitions
+#include <util/delay.h> // Functions for busy-wait delay loops
+#include <gpio.h>       // GPIO library for AVR-GCC
+
+
+// -----
+// This part is needed to use Arduino functions but also physical pin
+// names. We are using Arduino-style just to simplify the first lab.
+#include "Arduino.h"
+//#define PB5 5          // In Arduino world, PB5 is called "13"
+//#define PB0 0
+// -----
+
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: Main function where the program execution begins
+ * Purpose:  Toggle LEDs and use delay library.
+ * Returns:  none
+ **********************************************************************/
+int main(void)
+{
+    uint8_t led_value = 0;  // Local variable to keep LED status
+
+    // Set pins where LEDs are connected as output
+    // Ver 1: Arduino style
+    //pinMode(LED_GREEN, OUTPUT);
+    //pinMode(LED_RED, OUTPUT);
+
+    // Ver 2: Low-level (register) style
+    //DDRB |= (1<<LED_GREEN);
+    //DDRB |= (1<<LED_RED);
+
+    // Ver 3: Library function style
+    GPIO_mode_output(&DDRB, LED_GREEN);
+    GPIO_mode_output(&DDRB, LED_RED);
+    GPIO_mode_input_pullup(&DDRB, BUTTON);
+
+    // Infinite loop
+    while (1)
+    {
+        // Pause several milliseconds
+        _delay_ms(SHORT_DELAY);
+
+        // Change LED value
+        if (led_value == 0 && !GPIO_read(&PINB, BUTTON)) {
+            led_value = 1;
+            // Set pin(s) to HIGH
+            //digitalWrite(LED_GREEN, HIGH);
+            //digitalWrite(LED_RED, HIGH);
+            //PORTB |= (1<<LED_RED);
+            //PORTB |= (1<<LED_GREEN);
+            GPIO_write_high(&PORTB, LED_RED);
+            GPIO_write_high(&PORTB, LED_GREEN);
+            
+
+        }
+        else {
+            led_value = 0;
+            // Clear pin(s) to LOW
+            //digitalWrite(LED_GREEN, LOW);
+            //digitalWrite(LED_RED, LOW);
+            //PORTB &= ~(1<<LED_RED);
+            //PORTB &= ~(1<<LED_GREEN);
+            GPIO_write_low(&PORTB, LED_GREEN);
+            GPIO_write_low(&PORTB, LED_RED);
+
+        }
+    }
+
+    // Will never reach this
+    return 0;
 }
